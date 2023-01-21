296c02e18d44a936a2b4f94c1054c39fce9e0efa
AdityaBarshilia <barshiliaaditya7@gmail.com>
2023-01-21 15:38:02 +0530

    new

--- routes/user.route.js
+++ routes/user.route.js
@@ -13,9 +13,9 @@ app.post("/register", async (req, res) => {
     if (user) {
       res.status(400).send("User already exists");
     } else {
-        const hash = await argon2id.hash(password);
-        const user = await User.create({ name, email, password: hash });
-        return res.status(201).send({ message: "User created", user });
+      const hash = await argon2id.hash(password);
+      const user = await User.create({ name, email, password: hash });
+      return res.status(201).send({ message: "User created", user });
     }
   } catch (e) {
     res.status(400).send(e.message);
@@ -32,13 +32,22 @@ app.post("/login", async (req, res) => {
         return res.status(401).send({ message: "Incorrect password" });
       }
 
-      const token = jwt.sign({ id: user._id, name: user.name, email: user.email, created: user.createdAt }, config.secret, {
-        expiresIn: config.tokenLife,
-      });
+      const token = jwt.sign(
+        {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+          created: user.createdAt,
+        },
+        config.secret,
+        {
+          expiresIn: config.tokenLife,
+        }
+      );
 
       const response = {
         status: "Logged in",
-        token: token
+        token: token,
       };
 
       res.status(200).send(response);
@@ -50,19 +59,31 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.get("/getProfile", (req,res) => {
+app.get("/getProfile", (req, res) => {
   let { token } = req.headers;
 
   try {
     jwt.verify(token, config.secret, (err, verified) => {
-      if(err) return res.status(401).send("Unauthorized");
-      else{
+      if (err) return res.status(401).send("Unauthorized");
+      else {
         return res.status(200).send(verified);
       }
-    })
+    });
   } catch (error) {
     return res.status(400).send(error);
   }
-})
+});
+
+app.post("/calculate", (req, res) => {
+  const { aia, years, roi } = req.body;
+  try {
+    let MaturityVal = Math.floor(
+      aia * (((1 + roi / 100) ** years - 1) / (roi / 100))
+    );
+    res.send({MaturityVal});
+  } catch (e) {
+    res.send(e);
+  }
+});
 
 module.exports = app;
